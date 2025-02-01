e5d6c68e306711b791f183d04bdf946a8c13c803
Md Nur Islam <nurislamimran23@gmail.com>
2025-02-01 11:20:13 +0600

    rio project

---
+++ app.js
@@ -0,0 +1,185 @@
+const openIcon = document.getElementsByClassName("open-icon");
+
+const closeIcon = document.getElementsByClassName("close-icon");
+
+const accordion = document.getElementsByClassName("accordion-content");
+
+const accordionTittle = document.getElementsByClassName("accordion-tittle");
+
+openIcon[0].addEventListener("click", () => {
+  accordion[0].style.maxHeight = "500px";
+  accordion[0].style.marginTop = "30px";
+  accordion[0].style.opacity = "1";
+  openIcon[0].style.display = "none";
+  closeIcon[0].style.display = "block";
+  accordionTittle[0].style.color = "blue";
+  accordion[0].style.background = "white";
+});
+
+closeIcon[0].addEventListener("click", () => {
+  accordion[0].style.maxHeight = "0";
+  accordion[0].style.marginTop = "0";
+  accordion[0].style.opacity = "0";
+  openIcon[0].style.display = "block";
+  closeIcon[0].style.display = "none";
+  accordionTittle[0].style.color = "black";
+});
+
+// accordion 2
+
+const openIcon2 = document.getElementsByClassName("open-icon-2");
+
+const closeIcon2 = document.getElementsByClassName("close-icon-2");
+const accordion2 = document.getElementsByClassName("accordion-content-2");
+
+const accordionTittle2 = document.getElementsByClassName("accordion-tittle-2");
+
+openIcon2[0].addEventListener("click", () => {
+  accordion2[0].style.maxHeight = "500px";
+  accordion2[0].style.marginTop = "30px";
+  accordion2[0].style.opacity = "1";
+  openIcon2[0].style.display = "none";
+  closeIcon2[0].style.display = "block";
+  accordionTittle2[0].style.color = "blue";
+  accordion2[0].style.background = "white";
+});
+
+closeIcon2[0].addEventListener("click", () => {
+  accordion2[0].style.maxHeight = "0";
+  accordion2[0].style.marginTop = "0";
+  accordion2[0].style.opacity = "0";
+  openIcon2[0].style.display = "block";
+  closeIcon2[0].style.display = "none";
+  accordionTittle2[0].style.color = "black";
+});
+
+// accordion 3
+
+const openIcon3 = document.getElementsByClassName("open-icon-3");
+
+const closeIcon3 = document.getElementsByClassName("close-icon-3");
+const accordion3 = document.getElementsByClassName("accordion-content-3");
+
+const accordionTittle3 = document.getElementsByClassName("accordion-tittle-3");
+
+openIcon3[0].addEventListener("click", () => {
+  accordion3[0].style.maxHeight = "500px";
+  accordion3[0].style.marginTop = "30px";
+  accordion3[0].style.opacity = "1";
+  openIcon3[0].style.display = "none";
+  closeIcon3[0].style.display = "block";
+  accordionTittle3[0].style.color = "blue";
+  accordion3[0].style.background = "white";
+});
+
+closeIcon3[0].addEventListener("click", () => {
+  accordion3[0].style.maxHeight = "0";
+  accordion3[0].style.marginTop = "0";
+  accordion3[0].style.opacity = "0";
+  openIcon3[0].style.display = "block";
+  closeIcon3[0].style.display = "none";
+  accordionTittle3[0].style.color = "black";
+});
+
+//
+//
+//
+
+//  price section  accordion - 4/1
+//
+
+const openIcon4 = document.getElementsByClassName("open-icon-4");
+
+const closeIcon4 = document.getElementsByClassName("close-icon-4");
+const accordion4 = document.getElementsByClassName("accordion-content-4");
+
+const accordionTittle4 = document.getElementsByClassName("accordion-4");
+
+openIcon4[0].addEventListener("click", () => {
+  accordion4[0].style.maxHeight = "500px";
+  accordion4[0].style.marginTop = "30px";
+  accordion4[0].style.opacity = "1";
+  openIcon4[0].style.display = "none";
+  closeIcon4[0].style.display = "block";
+  accordionTittle4[0].style.color = "blue";
+  accordionTittle4[0].style.background = "white";
+});
+
+closeIcon4[0].addEventListener("click", () => {
+  accordion4[0].style.maxHeight = "0";
+  accordion4[0].style.marginTop = "0";
+  accordion4[0].style.opacity = "0";
+  openIcon4[0].style.display = "block";
+  closeIcon4[0].style.display = "none";
+  accordionTittle4[0].style.background = "#eeebeb3f";
+});
+
+//
+//
+//
+
+//  price section  accordion - 4/2
+//
+
+const openIcon5 = document.getElementsByClassName("open-icon-5");
+
+const closeIcon5 = document.getElementsByClassName("close-icon-5");
+const accordion5 = document.getElementsByClassName("accordion-content-5");
+
+const accordionTittle5 = document.getElementsByClassName("accordion-5");
+
+openIcon5[0].addEventListener("click", () => {
+  accordion5[0].style.maxHeight = "500px";
+  accordion5[0].style.marginTop = "30px";
+  accordion5[0].style.opacity = "1";
+  openIcon5[0].style.display = "none";
+  closeIcon5[0].style.display = "block";
+  accordionTittle5[0].style.color = "blue";
+  accordionTittle5[0].style.background = "white";
+});
+
+closeIcon5[0].addEventListener("click", () => {
+  accordion5[0].style.maxHeight = "0";
+  accordion5[0].style.marginTop = "0";
+  accordion5[0].style.opacity = "0";
+  openIcon5[0].style.display = "block";
+  closeIcon5[0].style.display = "none";
+  accordionTittle5[0].style.color = "black";
+  accordionTittle5[0].style.background = "#eeebeb3f";
+});
+//
+//
+//
+//
+//
+//
+//
+//  price section  accordion - 4/3
+//
+
+const openIcon6 = document.getElementsByClassName("open-icon-6");
+
+const closeIcon6 = document.getElementsByClassName("close-icon-6");
+const accordion6 = document.getElementsByClassName("accordion-content-6");
+
+const accordionTittle6 = document.getElementsByClassName("accordion-6");
+
+openIcon6[0].addEventListener("click", () => {
+  accordion6[0].style.maxHeight = "500px";
+  accordion6[0].style.marginTop = "30px";
+  accordion6[0].style.opacity = "1";
+  openIcon6[0].style.display = "none";
+  closeIcon6[0].style.display = "block";
+  accordionTittle6[0].style.color = "blue";
+  accordionTittle6[0].style.background = "white";
+});
+
+closeIcon6[0].addEventListener("click", () => {
+  accordion6[0].style.maxHeight = "0";
+  accordion6[0].style.marginTop = "0";
+  accordion6[0].style.opacity = "0";
+  openIcon6[0].style.display = "block";
+  closeIcon6[0].style.display = "none";
+  accordionTittle6[0].style.color = "black";
+  accordionTittle6[0].style.background = "#eeebeb3f";
+});
